3ff91353dc4afe72d61704cc6c8f21790e0eeb07
fabiostefani <fabiostefani@gmail.com>
2017-03-28 23:21:48 -0300

    DDD

---
+++ Stefani.EstudoDslInterna/DataUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stefani.EstudoDslInterna
+{
+    public static class DataUtil
+    {
+        public const int UM_DIA = 1;
+        public const int DOIS_DIAS = 2;
+        public const int TRES_DIAS = 3;
+        public const int QUATRO_DIAS = 4;
+
+        public static DateTime DaquiA(int numDias)
+        {
+            return DateTime.Now.AddDays(numDias).Date;
+        }
+    }
+}
---
+++ Stefani.EstudoDslInterna/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stefani.EstudoDslInterna
+{
+    class Program
+    {
+        static void Main()
+        {
+            var paulo = new Cliente("12345678960", "Paulo");
+            var locacao = Locacao.Para(paulo);
+
+            var videos = new List<Video>();
+
+            videos.Add(
+                new Video("Tropa de Elite")
+                .SetarNomeOriginal("Tropa de Elite")
+                .DoGenero("ação / crime / drama")
+                .Com("Wagner Moura, Caio Junqueira...")
+                .PequenoResumo("Filme retrata o trabalho do B.O.P.E. em ação..."));
+
+            videos.Add(
+                new Video("Os Simpsos")
+                .SetarNomeOriginal("Os Simpsos")
+                .DoGenero("Comédia")
+                .Com("Homer")
+                .PequenoResumo("só pra rir"));
+
+            locacao.Adicionar(videos)
+                .ParaDevolver(DataUtil.DaquiA(DataUtil.DOIS_DIAS))
+                .APagar();
+
+            GerenciadorLocador.Imprimir(locacao);
+
+            Console.ReadKey();
+        }
+    }
+}
